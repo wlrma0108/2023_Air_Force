e3258ccc065a3a12410f8a23485f153943da5f02
wlrma0108 <dlwlrma0108@gmail.com>
2023-03-26 12:38:12 +0900

    commit

--- Native/App.js
+++ Native/App.js
@@ -18,3 +18,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// 404 처리 미들웨어
+app.use(function(req, res, next) {
+  next(createError(404));
+  });
+  // 에러 핸들러
+  app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') ==='development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+  });
+  module.exports = app;
+  
