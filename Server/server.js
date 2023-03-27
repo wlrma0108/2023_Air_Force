5a6be76113c9b1f1520d2de4b3eed0f53d0beb6e
wlrma0108 <dlwlrma0108@gmail.com>
2023-03-27 11:24:31 +0900

    commit

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const logger = (req, res, next) => {
+    console.log(`Request ${req.path}`);
+    next();
+  };
+   
+  const auth = (req, res, next) => {
+    if (!isAdmin(req)) {
+      next(new Error("Not Authorized"));
+      return;
+    }
+    next();
+  };
+  
 app.get('/', (req, res) => {
     res.json({
         success: true,
@@ -11,3 +24,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`server is listening at localhost:${process.env.PORT}`);
 });
+
+
+
